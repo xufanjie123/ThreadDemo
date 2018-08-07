d589fddf945c93a482993d18f209b5e4a01de955
xufanjie123 <527719396@qq.com>
2018-08-07 23:57:40 +0800

    Initial commit

---
+++ src/main/java/SynchronizedDemo.java
@@ -0,0 +1,10 @@
+public class SynchronizedDemo {
+    public static void main(String[] args) {
+        synchronized (SynchronizedDemo.class) {
+        }
+        method();
+    }
+
+    private static void method() {
+    }
+}
